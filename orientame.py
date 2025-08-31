a9846027f73b7008c15249db7d2ef3397ce048c4
Milca <milcaotane02@gmail.com>
2025-08-31 13:30:54 -0500

    Update orientame.py

--- orientame.py
+++ orientame.py
@@ -3,3 +3,11 @@ import pandas as pd
 import numpy as np
 st.title('Descubre tu camino, diseña tu futuro con OrientaMe')
 
+import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime
+d = st.date_input(
+"Fecha de cumpleaños",
+datetime.date(2019, 7, 6))
+st.write('Tu cumpleños es:', d)
